79cc5bbfd6eb8521f3f279fd5971dc7d333740e0
Fandika Okdiba <fandika.okdiba@amartha.com>
2024-08-06 20:26:27 +0700

    add firevent click, add package-lock

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,31 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import { afterEach, describe, expect, it } from "vitest";
 import App from "./App";
 
 describe("App component", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
   it("renders App component and get page title", () => {
     render(<App />);
     const titleText = screen.getByText("Vite + React");
     expect(titleText).toBeDefined();
+  });
+
+  it("can click count button and get updated", () => {
+    // simulasi render component App
+    render(<App />);
+
+    // get button dengan isi teks count is 0
+    let countText = screen.getByText("count is 0");
+    expect(countText).toBeDefined();
+
+    // simulasi klik button
+    fireEvent.click(countText);
+
+    // get button dengan isi teks count is 1
+    countText = screen.getByText("count is 1");
+    expect(countText).toBeDefined();
   });
 });
